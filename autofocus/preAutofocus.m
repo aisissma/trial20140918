253bde43d7dc342b0eaef88df24fb1f25d17ba11
junkozuka <sengoku_sal2000@yahoo.co.jp>
2014-09-18 19:39:07 +0900

    branch1

--- autofocus/preAutofocus.m
+++ autofocus/preAutofocus.m
@@ -20,14 +20,18 @@ tSeq = 0:(count-1);
 zSeq = -difz:difz; %=sz:ez;
 xSeq = -difx:difx; %=sx:ex;
 ySeq = -dify:dify; %=sy:ey;
-% 
 
-imageNo = zeros([length(xSeq) length(ySeq) length(xSeq) length(tSeq)]);
+lenX = length(xSeq);
+lenY = length(ySeq);
+lenZ = length(zSeq);
+lenT = length(tSeq);
+
+imageNo = zeros([lenX lenY lenZ lenT]);
 nowNo = 1;
-for tLoop = tSeq,
-    for zLoop = zSeq,
-        for yLoop = ySeq,
-            for xLoop = xSeq,
+for tLoop = 1:lenT,
+    for zLoop = 1:lenZ,
+        for yLoop = 1:lenY,
+            for xLoop = 1:lenX,
                disp('take a picture'); 
                imageNo(xLoop, yLoop, zLoop, tLoop) = nowNo;
                nowNo = nowNo + 1;
